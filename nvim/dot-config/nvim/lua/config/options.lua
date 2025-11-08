e072aebe14591e0cedc8a2637a1c7069f5258c6b
khalil hadji <h-adji_tech@proton.me>
2025-11-08 13:18:16 +0100

    add lsp and formatter config

--- nvim/dot-config/nvim/lua/config/options.lua
+++ nvim/dot-config/nvim/lua/config/options.lua
@@ -6,6 +6,8 @@ vim.g.maplocalleader = "\\"
 vim.o.number = true
 vim.o.tabstop = 4
 vim.o.wrap = false
+vim.o.winborder = "rounded"
+vim.o.swapfile = false
 vim.o.relativenumber = true
 vim.keymap.set('n', '<leader>o', ':update<CR> :source<CR>')
 vim.keymap.set('n', 'zz', ':q<CR>')
@@ -14,9 +16,9 @@ vim.keymap.set('n', 'zz', ':q<CR>')
 -- increase startup-time. Remove this option if you want your OS clipboard to remain independent.
 -- See `:help 'clipboard'`
 vim.api.nvim_create_autocmd('UIEnter', {
-  callback = function()
-    vim.o.clipboard = 'unnamedplus'
-  end,
+	callback = function()
+		vim.o.clipboard = 'unnamedplus'
+	end,
 })
 
 -- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
@@ -57,10 +59,10 @@ vim.keymap.set({ 'n' }, '<A-l>', '<C-w>l')
 -- Highlight when yanking (copying) text.
 -- Try it with `yap` in normal mode. See `:h vim.hl.on_yank()`
 vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  callback = function()
-    vim.hl.on_yank()
-  end,
+	desc = 'Highlight when yanking (copying) text',
+	callback = function()
+		vim.hl.on_yank()
+	end,
 })
 
 -- [[ Create user commands ]]
@@ -68,9 +70,9 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 
 -- Create a command `:GitBlameLine` that print the git blame for the current line
 vim.api.nvim_create_user_command('GitBlameLine', function()
-  local line_number = vim.fn.line('.') -- Get the current line number. See `:h line()`
-  local filename = vim.api.nvim_buf_get_name(0)
-  print(vim.fn.system({ 'git', 'blame', '-L', line_number .. ',+1', filename }))
+	local line_number = vim.fn.line('.') -- Get the current line number. See `:h line()`
+	local filename = vim.api.nvim_buf_get_name(0)
+	print(vim.fn.system({ 'git', 'blame', '-L', line_number .. ',+1', filename }))
 end, { desc = 'Print the git blame for the current line' })
 
 -- [[ Add optional packages ]]
