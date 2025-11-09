e5825833b3c710721691da495f3e993bb38c7d2b
khalil hadji <h-adji_tech@proton.me>
2025-11-09 07:47:55 +0100

    separate options and keymaps ...

---
+++ nvim/dot-config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,34 @@
+local map = vim.keymap.set
+map("n", "zz",
+    function()
+        vim.cmd("q")
+    end,
+    { desc = "[Q]uit" }
+)
+map("n", "<leader>ll",
+    function()
+        vim.cmd("Lazy")
+    end,
+    { desc = "[L]azy Dashboard" }
+)
+map("n", "<leader>o",
+    function()
+        vim.cmd("update")
+        vim.cmd("source")
+        print("Config loaded")
+    end,
+    { desc = "S[o]urce config" }
+)
+-- map('n', '<leader>o', ':update<CR> :source<CR>')
+-- map('n', 'zz', ':q<CR>')
+-- Use <Esc> to exit terminal mode
+vim.keymap.set('t', '<Esc>', '<C-\\><C-n>')
+-- Map <A-j>, <A-k>, <A-h>, <A-l> to navigate between windows in any modes
+vim.keymap.set({ 't', 'i' }, '<A-h>', '<C-\\><C-n><C-w>h')
+vim.keymap.set({ 't', 'i' }, '<A-j>', '<C-\\><C-n><C-w>j')
+vim.keymap.set({ 't', 'i' }, '<A-k>', '<C-\\><C-n><C-w>k')
+vim.keymap.set({ 't', 'i' }, '<A-l>', '<C-\\><C-n><C-w>l')
+vim.keymap.set({ 'n' }, '<A-h>', '<C-w>h')
+vim.keymap.set({ 'n' }, '<A-j>', '<C-w>j')
+vim.keymap.set({ 'n' }, '<A-k>', '<C-w>k')
+vim.keymap.set({ 'n' }, '<A-l>', '<C-w>l')
--- nvim/dot-config/nvim/lua/config/options.lua
+++ nvim/dot-config/nvim/lua/config/options.lua
@@ -1,84 +1,22 @@
+-- To see documentation for an option, you can use `:h 'optionname'`, for example `:h 'number'`
 -- See `:help mapleader`
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
-
--- To see documentation for an option, you can use `:h 'optionname'`, for example `:h 'number'`
 vim.o.number = true
-vim.o.tabstop = 4
+vim.o.undofile = false
+vim.o.expandtab = true
+vim.o.shiftwidth = 4
+vim.o.softtabstop = 4
+vim.o.tabstop = 8
+-- vim.o.signcolumn = "no"
+-- Show <tab> and trailing spaces
+vim.o.list = false
 vim.o.wrap = false
 vim.o.winborder = "rounded"
 vim.o.swapfile = false
 vim.o.relativenumber = true
-vim.keymap.set('n', '<leader>o', ':update<CR> :source<CR>')
-vim.keymap.set('n', 'zz', ':q<CR>')
-
--- Sync clipboard between OS and Neovim. Schedule the setting after `UiEnter` because it can
--- increase startup-time. Remove this option if you want your OS clipboard to remain independent.
--- See `:help 'clipboard'`
-vim.api.nvim_create_autocmd('UIEnter', {
-	callback = function()
-		vim.o.clipboard = 'unnamedplus'
-	end,
-})
-
--- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
 vim.o.ignorecase = true
 vim.o.smartcase = true
-
--- Highlight the line where the cursor is on
 vim.o.cursorline = true
-
--- Minimal number of screen lines to keep above and below the cursor.
 vim.o.scrolloff = 20
-
--- Show <tab> and trailing spaces
-vim.o.list = true
-
--- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
--- instead raise a dialog asking if you wish to save the current file(s) See `:help 'confirm'`
 vim.o.confirm = true
-
--- [[ Set up keymaps ]] See `:h vim.keymap.set()`, `:h mapping`, `:h keycodes`
-
--- Use <Esc> to exit terminal mode
-vim.keymap.set('t', '<Esc>', '<C-\\><C-n>')
-
--- Map <A-j>, <A-k>, <A-h>, <A-l> to navigate between windows in any modes
-vim.keymap.set({ 't', 'i' }, '<A-h>', '<C-\\><C-n><C-w>h')
-vim.keymap.set({ 't', 'i' }, '<A-j>', '<C-\\><C-n><C-w>j')
-vim.keymap.set({ 't', 'i' }, '<A-k>', '<C-\\><C-n><C-w>k')
-vim.keymap.set({ 't', 'i' }, '<A-l>', '<C-\\><C-n><C-w>l')
-vim.keymap.set({ 'n' }, '<A-h>', '<C-w>h')
-vim.keymap.set({ 'n' }, '<A-j>', '<C-w>j')
-vim.keymap.set({ 'n' }, '<A-k>', '<C-w>k')
-vim.keymap.set({ 'n' }, '<A-l>', '<C-w>l')
-
--- [[ Basic Autocommands ]].
--- See `:h lua-guide-autocommands`, `:h autocmd`, `:h nvim_create_autocmd()`
-
--- Highlight when yanking (copying) text.
--- Try it with `yap` in normal mode. See `:h vim.hl.on_yank()`
-vim.api.nvim_create_autocmd('TextYankPost', {
-	desc = 'Highlight when yanking (copying) text',
-	callback = function()
-		vim.hl.on_yank()
-	end,
-})
-
--- [[ Create user commands ]]
--- See `:h nvim_create_user_command()` and `:h user-commands`
-
--- Create a command `:GitBlameLine` that print the git blame for the current line
-vim.api.nvim_create_user_command('GitBlameLine', function()
-	local line_number = vim.fn.line('.') -- Get the current line number. See `:h line()`
-	local filename = vim.api.nvim_buf_get_name(0)
-	print(vim.fn.system({ 'git', 'blame', '-L', line_number .. ',+1', filename }))
-end, { desc = 'Print the git blame for the current line' })
-
--- [[ Add optional packages ]]
--- Nvim comes bundled with a set of packages that are not enabled by
--- default. You can enable any of them by using the `:packadd` command.
-
--- For example, to add the "nohlsearch" package to automatically turn off search highlighting after
--- 'updatetime' and when going to insert mode
-vim.cmd('packadd! nohlsearch')
