5ae3bf7440771aa7977f19df33b59defd06406f7
khalil hadji <h-adji_tech@proton.me>
2025-11-10 16:48:54 +0100

    more plugins

--- nvim/dot-config/nvim/lua/plugins/mason.lua
+++ nvim/dot-config/nvim/lua/plugins/mason.lua
@@ -1,14 +1,56 @@
 return {
-    "mason-org/mason.nvim",
-    keys = {
-        {
-            "<leader>mm",
-            function()
-                vim.cmd("Mason")
-            end,
-            mode = { "n", "v" },
-            desc = "[M]ason",
-        }
-    },
-    opts = {}
+	{
+		"mason-org/mason.nvim",
+		keys = {
+			{
+				"<leader>mm",
+				function()
+					vim.cmd("Mason")
+				end,
+				mode = { "n", "v" },
+				desc = "[M]ason",
+			},
+		},
+		opts = {},
+	},
+	{
+		"mason-org/mason-lspconfig.nvim",
+		opts = {},
+	},
+
+	{
+		-- TODO: maybe replace with helper function from lazyvim.org
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		opts = {
+			ensure_installed = {
+				"lua_ls",
+				"ty",
+				"bash-language-server",
+				"marksman",
+				"markdownlint",
+				-- python stuff
+				-- "pyright",
+				-- "ruff",
+				-- golang stuff
+				-- "gopls",
+				-- "iferr",
+				-- "goimports",
+				-- "golines",
+				-- javascript
+				-- "typescript-language-server",
+				-- "biome",
+				-- "eslint-lsp",
+				-- "css-lsp",
+				-- "html-lsp",
+				-- "sql-formatter",
+				-- "dockerfile-language-server",
+				-- "docker-compose-language-service",
+				"prettierd",
+				"shellcheck",
+				"shfmt",
+				"stylua",
+				"beautysh",
+			},
+		},
+	},
 }
