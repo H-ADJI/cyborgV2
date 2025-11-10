10bc61c6b915562a18088fe79212a2e8ce99cc6f
khalil hadji <h-adji_tech@proton.me>
2025-11-10 08:31:50 +0100

    more plugins

---
+++ nvim/dot-config/nvim/lua/plugins/mini-jump.lua
@@ -0,0 +1,14 @@
+return {
+    'nvim-mini/mini.jump',
+    version = false,
+    config = function()
+        require("mini.jump").setup({
+            delay = {
+                -- Delay between jump and highlighting all possible jumps
+                highlight = 250,
+                -- Delay between jump and automatic stop if idle (no jump is done)
+                idle_stop = 2500,
+            },
+        })
+    end
+}
