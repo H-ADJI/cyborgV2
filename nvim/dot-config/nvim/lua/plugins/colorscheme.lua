ea40f2f1ac38ddf2b6c72dc53d094d9f0a745d50
khalil hadji <h-adji_tech@proton.me>
2025-11-15 14:48:58 +0100

    enhance transparency settings

--- nvim/dot-config/nvim/lua/plugins/colorscheme.lua
+++ nvim/dot-config/nvim/lua/plugins/colorscheme.lua
@@ -4,41 +4,9 @@ return {
     -- Optional, you don't have to run setup.
     config = function()
       require("transparent").setup({
-        -- table: default groups
-        groups = {
-          "Normal",
-          "NormalNC",
-          "Comment",
-          "Constant",
-          "Special",
-          "Identifier",
-          "Statement",
-          "PreProc",
-          "Type",
-          "Underlined",
-          "Todo",
-          "String",
-          "Function",
-          "Conditional",
-          "Repeat",
-          "Operator",
-          "Structure",
-          "LineNr",
-          "NonText",
-          "SignColumn",
-          "CursorLine",
-          "CursorLineNr",
-          "StatusLine",
-          "StatusLineNC",
-          "EndOfBuffer",
-        },
-        -- table: additional groups that should be cleared
         extra_groups = {
-          "WhichKeyNormal",
-          "WhichKeyTitle",
-          "WhichKeyBorder",
           "SnackBorder",
-          "SnackTitle",
+          "SnacksTitle",
           "SnackPrompt",
           "SnackPreview",
           "SnackResults",
@@ -62,12 +30,20 @@ return {
         -- table: groups you don't want to clear
         -- function: code to be executed after highlight groups are cleared
         -- Also the user event "TransparentClear" will be triggered
-        on_clear = function()
-          require("transparent").clear_prefix("SnacksNotifier")
-          -- require("transparent").clear_prefix("SnacksNotifier")
-          -- require("transparent").clear_prefix("Blink")
-        end,
+        on_clear = function() end,
       })
+      require("transparent").clear_prefix("SnacksNotifier")
+      require("transparent").clear_prefix("Which")
+      Snacks.toggle("transparency", {
+        name = "transparency",
+        get = function()
+          -- Check current diagnostic config
+          return vim.g.transparent_enabled
+        end,
+        set = function(state)
+          vim.cmd("TransparentToggle")
+        end,
+      }):map("<leader>ut")
     end,
   },
   {
