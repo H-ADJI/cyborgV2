1f06b05150301d3f04273c3b1abc686ad1154cd4
khalil hadji <h-adji_tech@proton.me>
2025-11-15 15:49:53 +0100

    fix treesitter diags

--- nvim/dot-config/nvim/lua/plugins/treesitter.lua
+++ nvim/dot-config/nvim/lua/plugins/treesitter.lua
@@ -8,9 +8,12 @@ return {
   -- },
   config = function()
     require("nvim-treesitter.configs").setup({
+      ignore_install = {},
+      sync_install = false,
       highlight = {
         enable = true,
       },
+      modules = {},
       ensure_installed = {
         "c",
         "lua",
@@ -21,6 +24,7 @@ return {
         "markdown",
         "markdown_inline",
         "python",
+        "json",
       },
       auto_install = true,
     })
