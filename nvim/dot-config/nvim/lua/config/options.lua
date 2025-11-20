7048d97789d81693d3921f6b25e3ddd3373b45bb
khalil hadji <h-adji_tech@proton.me>
2025-11-20 16:15:34 +0100

    fix autosave

--- nvim/dot-config/nvim/lua/config/options.lua
+++ nvim/dot-config/nvim/lua/config/options.lua
@@ -3,6 +3,7 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 vim.o.number = true
+vim.o.autowriteall = true
 vim.o.undofile = false
 vim.o.expandtab = true
 vim.o.shiftwidth = 4
--- nvim/dot-config/nvim/lua/plugins/auto-save.lua
+++ nvim/dot-config/nvim/lua/plugins/auto-save.lua
@@ -2,6 +2,6 @@ return {
   "Pocco81/auto-save.nvim",
   opts = {
     write_all_buffers = true,
-    trigger_events = { "InsertLeavePre", "TextChanged" },
+    trigger_events = { "InsertLeavePre", "TextChanged", "TextChangedI","BufLeave", },
   },
 }
