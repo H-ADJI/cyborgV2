5dd7983b17004b1f60afe350a4ede544e56fb51d
khalil hadji <h-adji_tech@proton.me>
2025-11-20 15:46:26 +0100

    fix remove cmts

--- nvim/dot-config/nvim/lua/plugins/colorscheme.lua
+++ nvim/dot-config/nvim/lua/plugins/colorscheme.lua
@@ -52,9 +52,6 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
-      -- require("tokyonight").setup({
-      --   transparent = true, -- Enable this to disable setting the background color
-      -- })
       vim.cmd([[colorscheme tokyonight]])
     end,
   },
